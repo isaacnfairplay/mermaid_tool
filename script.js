4188eb299f1338a86edbc087f9a72cf18c8c001f
isaac moore <isaaacmooreuky@gmail.com>
2023-12-31 15:31:40 -0500

    path format update curtesey gpt

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function initializeMermaid(theme) {
 }
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('mermaid_tool/service-worker.js').then(function(registration) {
+      navigator.serviceWorker.register('/mermaid_tool/service-worker.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
